365700338738b092672d582716b3c5a1af5b1d09
Amalia Haible <florian@haible.de>
2025-08-31 23:26:26 +0200

    color lol?

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -17,22 +17,22 @@ return {
         name = "nightfox",
         config = function()
             require("nightfox").setup({
-                -- palettes = {
-                --     -- Custom duskfox with black background
-                --     nordfox = {
-                --         -- bg0 = "#282c34",
-                --         bg1 = "#282c34",
-                --         -- bg0 = "#FF0000",
-                --         -- bg2 = "#00FF00",
-                --         -- bg3 = "#0000FF",
-                --         -- bg4 = "#010101",
-                --
-                --         -- bg2 = "#282c34",
-                --         -- bg0 = "#1d1d2b", -- Alt backgrounds (floats, statusline, ...)
-                --         -- bg3 = "#121820", -- 55% darkened from stock
-                --         -- sel0 = "#131b24", -- 55% darkened from stock
-                --     },
-                -- },
+                palettes = {
+                    -- Custom duskfox with black background
+                    nordfox = {
+                        -- bg0 = "#282c34",
+                        bg1 = "#282c34",
+                        -- bg0 = "#FF0000",
+                        -- bg2 = "#00FF00",
+                        -- bg3 = "#0000FF",
+                        -- bg4 = "#010101",
+
+                        -- bg2 = "#282c34",
+                        -- bg0 = "#1d1d2b", -- Alt backgrounds (floats, statusline, ...)
+                        -- bg3 = "#121820", -- 55% darkened from stock
+                        -- sel0 = "#131b24", -- 55% darkened from stock
+                    },
+                },
                 -- specs = {
                 --   duskfox = {
                 --     inactive = "#090909", -- Slightly lighter then black background
